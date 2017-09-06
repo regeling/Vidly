1edef5eda48cde00d0e14dedfd2369d836e96868
regel <regel@DESKTOP-8P226AN>
2017-09-06 11:02:06 +0200

    Initial

---
+++ Vidly/Controllers/MovieController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MovieController : Controller
+    {
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Name = "Shrek" },
+                new Movie { Name = "Wall-e" }
+            };
+        }
+    }
+}
