be39762f98a9e96fa8ef980d101fed5141213c84
regel <regel@DESKTOP-8P226AN>
2017-09-07 11:31:35 +0200

    Working Before Exercise

--- Vidly/Controllers/MovieController.cs
+++ Vidly/Controllers/MovieController.cs
@@ -1,19 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
+﻿using System.Data.Entity;
 using System.Web.Mvc;
-using Vidly.Migrations;
+using System.Linq;
 using Vidly.Models;
-using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
-    public class MoviesController : Controller
+    public class MovieController : Controller
     {
-        private ApplicationDbContext _context;
-
-        public MoviesController()
+        private readonly ApplicationDbContext _context;
+        public MovieController()
         {
             _context = new ApplicationDbContext();
         }
@@ -23,93 +18,20 @@ namespace Vidly.Controllers
             _context.Dispose();
         }
 
-        public ViewResult Index()
+        public ActionResult Index()
         {
-            var movies = _context.Movies.Include(m => m.Genre).ToList();
-
+            var movies = _context.Movies.Include(c => c.Genre).ToList();
             return View(movies);
         }
 
-        public ViewResult New()
-        {
-            var genres = _context.Genres.ToList();
-
-            var viewModel = new NewMovieViewModel
-            {
-                GenreTypes = genres
-            };
-
-            return View("MovieForm", viewModel);
-        }
-
-        public ActionResult Edit(int id)
-        {
-            var movie = _context.Movies.SingleOrDefault(c => c.Id == id);
-
-            if (movie == null)
-                return HttpNotFound();
-
-            var viewModel = new NewMovieViewModel
-            {
-                Movie = movie,
-                GenreTypes = _context.Genres.ToList()
-            };
-
-            return View("MovieForm", viewModel);
-        }
-
-
         public ActionResult Details(int id)
         {
-            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+            var movies = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
 
-            if (movie == null)
+            if (movies == null)
                 return HttpNotFound();
 
-            return View(movie);
-
-        }
-
-
-        // GET: Movies/Random
-        public ActionResult Random()
-        {
-            var movie = new Movie() { Name = "Shrek!" };
-            var customers = new List<Customer>
-            {
-                new Customer { Name = "Customer 1" },
-                new Customer { Name = "Customer 2" }
-            };
-
-            var viewModel = new RandomMovieViewModel
-            {
-                Movie = movie,
-                Customers = customers
-            };
-
-            return View(viewModel);
-        }
-
-        [HttpPost]
-        public ActionResult Save(Movie movie)
-        {
-            if (movie.Id == 0)
-            {
-                movie.DateAdded = DateTime.Now;
-                _context.Movies.Add(movie);
-            }
-            else
-            {
-                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
-                movieInDb.Name = movie.Name;
-                movieInDb.GenreId = movie.GenreId;
-                movieInDb.NumberInStock = movie.NumberInStock;
-                movieInDb.ReleaseDate = movie.ReleaseDate;
-            }
-
-            _context.SaveChanges();
-
-            return RedirectToAction("Index", "Movie");
+            return View(movies);
         }
     }
 }
