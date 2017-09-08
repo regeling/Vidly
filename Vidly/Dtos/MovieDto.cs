ca38dcd87dfb75292b6e2c7b7ad98289fe028391
Rick Regeling <regeling@asterisque.nl>
2017-09-08 11:25:00 +0200

    Before API using jQuery

---
+++ Vidly/Dtos/MovieDto.cs
@@ -0,0 +1,30 @@
+﻿namespace Vidly.Dtos
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using Models;
+
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public Genre Genre { get; set; }
+
+        [Required]
+        public byte GenreId { get; set; }
+
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required]
+        public DateTime DateAdded { get; set; }
+
+        [Required]
+        [Range(1, 20)]
+        public byte NumberInStock { get; set; }
+    }
+}
